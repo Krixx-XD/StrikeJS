1778968f4f4c8398ab9d0618cc91d3dd3fea1aad
The DV7s <161109688+TheDV7s@users.noreply.github.com>
2024-07-12 10:46:33 +0500

    Create init.lua

---
+++ strike/init.lua
@@ -0,0 +1,8 @@
+--[[
+  THIS SOURCE IS NOT FREE TO USE,  IF YOU WANT TO USE IT YOU MUST
+  CONTACT @AzuzyNotAzu On DISCORD,  @.r4t3. is HIS DISCORD, 
+  COPYRIGHT DV7 Team AND AzuzyNotAzu.
+]]--
+
+-- azuzy obfuscator v1 (weak)
+local YTAzNDkzNTg5NzQzOTY = require("amljMzQ5NTgzODk0NTg2MzY")
